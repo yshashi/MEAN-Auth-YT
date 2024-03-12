4095699b6fc1112ce495f2062e058008b4aab64a
Sashikumar <yshashi30@gmail.com>
2024-03-12 20:21:21 +0800

    feat: initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export default class HomeComponent {
+}
